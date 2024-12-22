32923722514019c107691032b5c97fa01013e61a
Emma Goto <33337378+emgoto@users.noreply.github.com>
2024-12-22 12:00:03 +0900

    Initial commit

--- main.ts
+++ main.ts
@@ -1,13 +1,15 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
+import { App, FileSystemAdapter, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import * as fs from 'fs';
+import * as path from 'path';
 
 interface MyPluginSettings {
-	mySetting: string;
+	folderToCopy: string;
+	destination: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	folderToCopy: '',
+	destination: ''
 }
 
 export default class MyPlugin extends Plugin {
@@ -16,71 +18,20 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+			id: 'push-folder-command',
+			name: 'Push folder',
+			callback: async () => {
+				await this.copyFolderContents(this.settings.folderToCopy, this.settings.destination);
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new SettingTab(this.app, this));
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
@@ -89,25 +40,53 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	getVaultPath() {	
+		let adapter = this.app.vault.adapter;
+		if (adapter instanceof FileSystemAdapter) {
+			return adapter.getBasePath();
+		}
+		return null;
+	}
+
+	async copyFolderContents(sourceFolder: string, destinationFolder: string) {
+		if (!destinationFolder) {
+			new Notice('Must set destination folder');
+			return;
+		}
+
+		const copy = async (src: string, dest: string) => {
+			const entries = await fs.promises.readdir(src, { withFileTypes: true });			
+			await fs.promises.mkdir(dest, { recursive: true });
+
+			for (const entry of entries) {
+				const srcPath = path.join(src, entry.name);
+				const destPath = path.join(dest, entry.name);
+
+				if (entry.isDirectory()) {
+					await copy(srcPath, destPath); // Recursively copy directories
+				} else {
+					await fs.promises.copyFile(srcPath, destPath); // Copy files
+				}
+			}
+		};
+
+		try {
+			const vaultPath = this.getVaultPath();
+			if (!vaultPath) {
+				new Notice('Failed to get vault path');
+				return;
+			}
+			await copy(path.join(vaultPath, sourceFolder), destinationFolder);
+			new Notice('Folder copied successfully!');
+		} catch (error) {
+			console.error('Error copying folder:', error);
+			new Notice('Failed to copy folder. Check console for details.');
+		}
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
+class SettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
 	constructor(app: App, plugin: MyPlugin) {
@@ -119,15 +98,26 @@ class SampleSettingTab extends PluginSettingTab {
 		const {containerEl} = this;
 
 		containerEl.empty();
+	
+		new Setting(containerEl)
+			.setName('Folder to copy')
+			.setDesc('Select a folder inside your Obsidian vault')
+			.addText(text => text
+				.setValue(this.plugin.settings.folderToCopy)
+				.setPlaceholder('Enter folder path')
+				.onChange(async (value) => {
+					this.plugin.settings.folderToCopy = value;
+					await this.plugin.saveSettings();
+				}));
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Destination')
+			.setDesc('Where to copy your folder to. Note this will override anything in the destination folder. Think of it like a one-way push.')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setValue(this.plugin.settings.destination)
+				.setPlaceholder('/Users/emma/src/my-blog/content/')
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.destination = value;
 					await this.plugin.saveSettings();
 				}));
 	}
