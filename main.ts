31b67eee9bbcf38ef298688ee7cece710f88f817
Jordan Le Lay <jordanlelay@protonmail.com>
2025-09-15 23:43:23 +0200

    style: prettier auto fix

--- main.ts
+++ main.ts
@@ -1,6 +1,13 @@
-import { App, FileSystemAdapter, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-import * as fs from 'fs';
-import * as path from 'path';
+import {
+	App,
+	FileSystemAdapter,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
+import * as fs from "fs";
+import * as path from "path";
 
 interface MyPluginSettings {
 	folderToCopy: string;
@@ -8,9 +15,9 @@ interface MyPluginSettings {
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	folderToCopy: '',
-	destination: ''
-}
+	folderToCopy: "",
+	destination: "",
+};
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
@@ -20,11 +27,14 @@ export default class MyPlugin extends Plugin {
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'push-folder-command',
-			name: 'Push folder',
+			id: "push-folder-command",
+			name: "Push folder",
 			callback: async () => {
-				await this.copyFolderContents(this.settings.folderToCopy, this.settings.destination);
-			}
+				await this.copyFolderContents(
+					this.settings.folderToCopy,
+					this.settings.destination
+				);
+			},
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
@@ -34,14 +44,18 @@ export default class MyPlugin extends Plugin {
 	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
 
-	getVaultPath() {	
+	getVaultPath() {
 		let adapter = this.app.vault.adapter;
 		if (adapter instanceof FileSystemAdapter) {
 			return adapter.getBasePath();
@@ -51,12 +65,14 @@ export default class MyPlugin extends Plugin {
 
 	async copyFolderContents(sourceFolder: string, destinationFolder: string) {
 		if (!destinationFolder) {
-			new Notice('Must set destination folder');
+			new Notice("Must set destination folder");
 			return;
 		}
 
 		const copy = async (src: string, dest: string) => {
-			const entries = await fs.promises.readdir(src, { withFileTypes: true });			
+			const entries = await fs.promises.readdir(src, {
+				withFileTypes: true,
+			});
 			await fs.promises.mkdir(dest, { recursive: true });
 
 			for (const entry of entries) {
@@ -74,14 +90,14 @@ export default class MyPlugin extends Plugin {
 		try {
 			const vaultPath = this.getVaultPath();
 			if (!vaultPath) {
-				new Notice('Failed to get vault path');
+				new Notice("Failed to get vault path");
 				return;
 			}
 			await copy(path.join(vaultPath, sourceFolder), destinationFolder);
-			new Notice('Folder copied successfully!');
+			new Notice("Folder copied successfully!");
 		} catch (error) {
-			console.error('Error copying folder:', error);
-			new Notice('Failed to copy folder. Check console for details.');
+			console.error("Error copying folder:", error);
+			new Notice("Failed to copy folder. Check console for details.");
 		}
 	}
 }
@@ -95,30 +111,36 @@ class SettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
-	
-		new Setting(containerEl)
-			.setName('Folder to copy')
-			.setDesc('Select a folder inside your Obsidian vault')
-			.addText(text => text
-				.setValue(this.plugin.settings.folderToCopy)
-				.setPlaceholder('Enter folder path')
-				.onChange(async (value) => {
-					this.plugin.settings.folderToCopy = value;
-					await this.plugin.saveSettings();
-				}));
 
 		new Setting(containerEl)
-			.setName('Destination')
-			.setDesc('Where to copy your folder to. Note this will override anything in the destination folder. Think of it like a one-way push.')
-			.addText(text => text
-				.setValue(this.plugin.settings.destination)
-				.setPlaceholder('/Users/emma/src/my-blog/content/')
-				.onChange(async (value) => {
-					this.plugin.settings.destination = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Folder to copy")
+			.setDesc("Select a folder inside your Obsidian vault")
+			.addText((text) =>
+				text
+					.setValue(this.plugin.settings.folderToCopy)
+					.setPlaceholder("Enter folder path")
+					.onChange(async (value) => {
+						this.plugin.settings.folderToCopy = value;
+						await this.plugin.saveSettings();
+					})
+			);
+
+		new Setting(containerEl)
+			.setName("Destination")
+			.setDesc(
+				"Where to copy your folder to. Note this will override anything in the destination folder. Think of it like a one-way push."
+			)
+			.addText((text) =>
+				text
+					.setValue(this.plugin.settings.destination)
+					.setPlaceholder("/Users/emma/src/my-blog/content/")
+					.onChange(async (value) => {
+						this.plugin.settings.destination = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
